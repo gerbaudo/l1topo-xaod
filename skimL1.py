b13d0445132cfa2a97798c7ca529e6beac653262
gerbaudo <davide.gerbaudo@gmail.com>
2016-02-10 11:39:19 +0100

    move to main

--- skimL1.py
+++ skimL1.py
@@ -1,133 +1,135 @@
 #!/usr/bin/env python
 import sys
 from math import pi,sqrt,cos
-from ctypes import *
 import array
 
 import ROOT
-ROOT.gROOT.Macro( '$ROOTCOREDIR/scripts/load_packages.C' )
-#ROOT.gROOT.ProcessLine(".L L1MuonRoI_cxx.so")
-ROOT.gROOT.LoadMacro( "L1MuonRoI.cxx+" )
 
-# Initialize the xAOD infrastructure:
-if(not ROOT.xAOD.Init().isSuccess()): print "Failed xAOD.Init()"
+def main():
+    # init: load packages, setup xAOD infrastructure
+    ROOT.gROOT.SetBatch(1)
+    ROOT.gROOT.Macro('$ROOTCOREDIR/scripts/load_packages.C')
+    ROOT.gROOT.LoadMacro('L1MuonRoI.cxx+')
+    if(not ROOT.xAOD.Init().isSuccess()): print "Failed xAOD.Init()"
 
-inputFiles = sys.argv[1].split(',')
-print "inputFiles = ", inputFiles
-fileName=inputFiles[0]
-if len(fileName) == 0 :
-    print "Please provide input"
-    exit()
+    # Read input tree
+    inputFiles = sys.argv[1].split(',')
+    print "inputFiles = ", inputFiles
+    fileName=inputFiles[0]
+    if len(fileName) == 0 :
+        print "Please provide input"
+        exit()
 
-process_all_events = False
-filter_events = False
-verbose = False
+    process_all_events = False
+    filter_events = False
+    verbose = False
 
-######### Read input tree
+    treeName = "CollectionTree" # default when making transient tree anyway
+    ch = ROOT.TChain(treeName)
+    for input_file in sys.argv[1:]:
+        ch.Add(input_file)
+        print input_file
+    t = ROOT.xAOD.MakeTransientTree( ch ) #f, treeName)
 
-treeName = "CollectionTree" # default when making transient tree anyway
+    initialize_trigger_decision_tool()
+    trigList = get_muon_triggers_to_process()
 
-ch = ROOT.TChain(treeName)
-for i in range(1,len(sys.argv)) :
-    ch.Add(sys.argv[i])
-    print sys.argv[i]
+    from OTriggerHLT import OTriggerHLT
+    HLT = OTriggerHLT()
+    counters = dict((trig, 0) for trig in trigList)
+    passTrig = {}
 
-t = ROOT.xAOD.MakeTransientTree( ch ) #f, treeName)
+    # create the output tree with its branches
+    fOut = ROOT.TFile("tmptrig.root","RECREATE")
+    tOut = ROOT.TTree("trig","truth")
+    passTrig = {}
+    for trig in trigList :
+        passTrig[trig] = array.array("i",(0 for i in range(0,2)))
+        tOut.Branch(str(trig), passTrig[trig],str(trig)+"/I")
 
-######### Initialize TrigDecision
+    eventNumber = array.array("i",(0 for i in range(0,2)))
+    tOut.Branch("eventNumber", eventNumber,"eventNumber/I")
+    runNumber = array.array("i",(0 for i in range(0,2)))
+    tOut.Branch("runNumber", runNumber,"runNumber/I")
 
-ROOT.gROOT.ProcessLine(
-"  TrigConf::xAODConfigTool configTool( \"xAODConfigTool\" );\
-  ToolHandle<TrigConf::ITrigConfigTool> handle(&configTool);\
-  handle->initialize().ignore();\
-  Trig::TrigDecisionTool  trigDecTool(\"TrigDecTool\");\
-  trigDecTool.setProperty(\"ConfigTool\",handle).ignore();\
-  trigDecTool.setProperty(\"TrigDecisionKey\",\"xTrigDecision\").ignore();\
-  trigDecTool.setProperty(\"OutputLevel\", MSG::INFO).ignore();\
-  trigDecTool.initialize().ignore();" )
+    l1muons = ROOT.vector('L1MuonRoI')(10)
+    tOut.Branch('l1muons',  l1muons  )
+    l1muonsn= array.array("i",(0 for i in range(0,1)))
+    tOut.Branch('l1muons_n',  l1muonsn, "l1muons_n/I"  )
 
+    topomuons = ROOT.vector('TLorentzVector')(10)
+    tOut.Branch('topomuons',  topomuons  )
+    topomuonsn= array.array("i",(0 for i in range(0,1)))
+    tOut.Branch('topomuons_n',  topomuonsn, "topomuons_n/I"  )
 
-
-
-trigList = [
-    "L1_MU4","L1_MU6",
-    # "L1_MU10","L1_MU11","L1_MU15","L1_MU20",
-    # "L1_2MU4","L1_2MU6","L1_2MU10","L1_MU10_2MU4","L1_MU10_2MU6","L1_LFV-MU",
-    "L1_LFV-MU-topo",
-    # "L1_3MU6","L1_3MU4","L1_MU6_3MU4",
-    # "HLT_mu4",  "HLT_mu6", "HLT_mu10", "HLT_mu18","HLT_mu24",
-    # "HLT_2mu14",
-    # "HLT_2mu10",
-    # "HLT_mu18_mu8noL1", "HLT_mu18_2mu4noL1",
-
-    # "HLT_3mu4",
-    # "HLT_mu4_2mu6",
-    # "HLT_3mu6",
-    # "HLT_3mu6_msonly",
-    # "HLT_3mu4_bTau", "HLT_3mu6_bTau",
-
-    # "HLT_mu20_msonly_mu6noL1_msonly_nscan05",
-    # "HLT_mu20_mu6noL1_nscan03",
-
-    # #"HLT_mu11_2mu4noL1_nscan03",
-    # "HLT_mu11_2mu4noL1_nscan03_L1MU11_2MU6",
-    # 'HLT_mu11_llns_2mu4noL1_nscan03_L1MU11_2MU6',
-    # "HLT_mu11_L1MU10_2mu4noL1_nscan03_L1MU10_2MU6",
-
-    # "HLT_noalg_eb_L1PhysicsLow_noPS", #L1_2MU6 , L1_3MU4, L1_MU20,
-    # "HLT_noalg_eb_L1PhysicsHigh_noPS", # 3MU6
-    # "HLT_eb_low_L1RD2_FILLED", # L1_MU6' L1_MU4_J12'
-    # "HLT_eb_high_L1RD2_FILLED"  # L1_2MU4', L1_MU15 L1_MU6_2MU4', 'L1_MU6_J20'  'L1_EM7_MU10', 'L1_EM15_MU4',
-]
-
-from OTriggerHLT import OTriggerHLT
-HLT = OTriggerHLT()
+    recomuons = ROOT.vector('TLorentzVector')(10)
+    tOut.Branch('recomuons',  recomuons  )
+    recomuonsn= array.array("i",(0 for i in range(0,1)))
+    tOut.Branch('recomuons_n',  recomuonsn, "recomuons_n/I"  )
+    recotype = ROOT.vector('int')(10)
+    tOut.Branch('recotype',  recotype  )
 
 
-counters = {}
-for trig in trigList :
-    counters[trig] = 0
+    # Start processing events
+    print( "Number of input events: %s" % t.GetEntries() )
+    numEntriesToProcess = max([10, t.GetEntries()]) if process_all_events else min([10, t.GetEntries()])
+    print("About to process %d entries" % numEntriesToProcess)
+    mev2gev=1.0E-3
 
-passTrig = {}
+    for entry in xrange(numEntriesToProcess):
+        t.GetEntry( entry )
+        HLT.setEvent(t)
+        emulated = HLT.emulateDecision("L1_LFV-MU")
 
-fOut = ROOT.TFile("tmptrig.root","RECREATE")
+        eventNumber[0] = t.EventInfo.eventNumber()
+        runNumber[0] = t.EventInfo.runNumber()
+        l1met = t.LVL1EnergySumRoI
+        l1metp4 = get_l1met_p4(l1met, verbose)
+        l1jets = t.LVL1JetRoIs
+        eventN = t.EventInfo.eventNumber()
+        if filter_events and skip_run_event(runNumber[0], eventN):
+            continue
+        print("%d / %d" % (runNumber[0], eventNumber[0]))
+        # met->Ex() << "  " << met->Ey() << "  " << met->Et()
+        # print("l1met: ex %.2f ey %2.f et %2.f" % (l1met.exMiss(), l1met.eyMiss(), l1metp4.Et()))
+        if verbose:
+            print_l1met(l1met, l1metp4)
+            print_l1jets(l1jets)
 
-tOut = ROOT.TTree("trig","truth")
-passTrig = {}
-for trig in trigList :
-    passTrig[trig] = array.array("i",(0 for i in range(0,2)))
-    tOut.Branch(str(trig), passTrig[trig],str(trig)+"/I")
+        l1emtaus= t.LVL1EmTauRoIs
+        if verbose:
+            print_l1emtaus(l1emtaus)
 
-eventNumber = array.array("i",(0 for i in range(0,2)))
-tOut.Branch("eventNumber", eventNumber,"eventNumber/I")
-runNumber = array.array("i",(0 for i in range(0,2)))
-tOut.Branch("runNumber", runNumber,"runNumber/I")
+        for trig in trigList :
+            passTrig[trig][0] = 0
+            if ROOT.trigDecTool.isPassed( trig ):
+                passTrig[trig][0] = 1
+        passTrig["L1_LFV-MU-topo"][0] = emulated
 
-#l1muons = ROOT.vector('TLorentzVector')(10)
-l1muons = ROOT.vector('L1MuonRoI')(10)
-tOut.Branch('l1muons',  l1muons  )
-l1muonsn= array.array("i",(0 for i in range(0,1)))
-tOut.Branch('l1muons_n',  l1muonsn, "l1muons_n/I"  )
+        l1muons.clear()
+        for l1mu in t.LVL1MuonRoIs:
+            l1muons.push_back(l1muonroi2l1muon(l1mu))
+        l1muonsn[0] = len(l1muons)
 
-topomuons = ROOT.vector('TLorentzVector')(10)
-tOut.Branch('topomuons',  topomuons  )
-topomuonsn= array.array("i",(0 for i in range(0,1)))
-tOut.Branch('topomuons_n',  topomuonsn, "topomuons_n/I"  )
+        topomuons.clear()
+        for toporoi in HLT.topoRoIs:
+            topomuons.push_back(toporoi2mu(toporoi))
+        topomuonsn[0] = len(topomuons)
 
-recomuons = ROOT.vector('TLorentzVector')(10)
-tOut.Branch('recomuons',  recomuons  )
-recomuonsn= array.array("i",(0 for i in range(0,1)))
-tOut.Branch('recomuons_n',  recomuonsn, "recomuons_n/I"  )
-recotype = ROOT.vector('int')(10)
-tOut.Branch('recotype',  recotype  )
+        recomuons.clear()
+        recotype.clear()
+        ntup_recomuons = sorted(filter_muons(t.Muons), key=lambda m : m.pt(), reverse=True)
+        for mu in ntup_recomuons:
+            recomuons.push_back(muon2recomuon(mu))
+            recotype.push_back(mu.algo_type)
+        recomuonsn[0] = len(recomuons)
 
+        tOut.Fill()
 
-################################################################
-
-print( "Number of input events: %s" % t.GetEntries() )
-numEntriesToProcess = max([10, t.GetEntries()]) if process_all_events else min([10, t.GetEntries()])
-print("About to process %d entries" % numEntriesToProcess)
-mev2gev=1.0E-3
+    print "Write out trig ntuple ", fOut.GetName() , " with ", tOut.GetEntries(), " events"
+    fOut.Write()
+    fOut.Close()
+    exit(0)
 
 def get_l1met_p4(l1met, verbose=False):
     mev2gev = 1.0E-3
@@ -231,57 +233,48 @@ def muon2recomuon(mu):
     recomuon.SetPtEtaPhiM(mu.pt(), mu.eta(), mu.phi(), mu_mass)
     return recomuon
 
-for entry in xrange(numEntriesToProcess):
-    t.GetEntry( entry )
-    HLT.setEvent(t)
-    emulated = HLT.emulateDecision("L1_LFV-MU")
+def get_muon_triggers_to_process():
+    return [
+        "L1_MU4","L1_MU6",
+        # "L1_MU10","L1_MU11","L1_MU15","L1_MU20",
+        # "L1_2MU4","L1_2MU6","L1_2MU10","L1_MU10_2MU4","L1_MU10_2MU6","L1_LFV-MU",
+        "L1_LFV-MU-topo",
+        # "L1_3MU6","L1_3MU4","L1_MU6_3MU4",
+        # "HLT_mu4",  "HLT_mu6", "HLT_mu10", "HLT_mu18","HLT_mu24",
+        # "HLT_2mu14",
+        # "HLT_2mu10",
+        # "HLT_mu18_mu8noL1", "HLT_mu18_2mu4noL1",
 
-    eventNumber[0] = t.EventInfo.eventNumber()
-    runNumber[0] = t.EventInfo.runNumber()
-    l1met = t.LVL1EnergySumRoI
-    l1metp4 = get_l1met_p4(l1met, verbose)
-    l1jets = t.LVL1JetRoIs
-    eventN = t.EventInfo.eventNumber()
-    if filter_events and skip_run_event(runNumber[0], eventN):
-        continue
-    print("%d / %d" % (runNumber[0], eventNumber[0]))
-    # met->Ex() << "  " << met->Ey() << "  " << met->Et()
-    # print("l1met: ex %.2f ey %2.f et %2.f" % (l1met.exMiss(), l1met.eyMiss(), l1metp4.Et()))
-    if verbose:
-        print_l1met(l1met, l1metp4)
-        print_l1jets(l1jets)
+        # "HLT_3mu4",
+        # "HLT_mu4_2mu6",
+        # "HLT_3mu6",
+        # "HLT_3mu6_msonly",
+        # "HLT_3mu4_bTau", "HLT_3mu6_bTau",
 
-    l1emtaus= t.LVL1EmTauRoIs
-    if verbose:
-        print_l1emtaus(l1emtaus)
+        # "HLT_mu20_msonly_mu6noL1_msonly_nscan05",
+        # "HLT_mu20_mu6noL1_nscan03",
 
-    for trig in trigList :
-        passTrig[trig][0] = 0
-        if ROOT.trigDecTool.isPassed( trig ):
-            passTrig[trig][0] = 1
-    passTrig["L1_LFV-MU-topo"][0] = emulated
+        # #"HLT_mu11_2mu4noL1_nscan03",
+        # "HLT_mu11_2mu4noL1_nscan03_L1MU11_2MU6",
+        # 'HLT_mu11_llns_2mu4noL1_nscan03_L1MU11_2MU6',
+        # "HLT_mu11_L1MU10_2mu4noL1_nscan03_L1MU10_2MU6",
 
-    l1muons.clear()
-    for l1mu in t.LVL1MuonRoIs:
-        l1muons.push_back(l1muonroi2l1muon(l1mu))
-    l1muonsn[0] = len(l1muons)
+        # "HLT_noalg_eb_L1PhysicsLow_noPS", #L1_2MU6 , L1_3MU4, L1_MU20,
+        # "HLT_noalg_eb_L1PhysicsHigh_noPS", # 3MU6
+        # "HLT_eb_low_L1RD2_FILLED", # L1_MU6' L1_MU4_J12'
+        # "HLT_eb_high_L1RD2_FILLED"  # L1_2MU4', L1_MU15 L1_MU6_2MU4', 'L1_MU6_J20'  'L1_EM7_MU10', 'L1_EM15_MU4',
+        ]
+def initialize_trigger_decision_tool():
+    cmd = """TrigConf::xAODConfigTool configTool( "xAODConfigTool" );
+ToolHandle<TrigConf::ITrigConfigTool> handle(&configTool);
+handle->initialize().ignore();
+Trig::TrigDecisionTool  trigDecTool("TrigDecTool");
+trigDecTool.setProperty("ConfigTool",handle).ignore();
+trigDecTool.setProperty("TrigDecisionKey","xTrigDecision").ignore();
+trigDecTool.setProperty("OutputLevel", MSG::INFO).ignore();
+trigDecTool.initialize().ignore();
+"""
+    ROOT.gROOT.ProcessLine(cmd)
 
-    topomuons.clear()
-    for toporoi in HLT.topoRoIs:
-        topomuons.push_back(toporoi2mu(toporoi))
-    topomuonsn[0] = len(topomuons)
-
-    recomuons.clear()
-    recotype.clear()
-    ntup_recomuons = sorted(filter_muons(t.Muons), key=lambda m : m.pt(), reverse=True)
-    for mu in ntup_recomuons:
-        recomuons.push_back(muon2recomuon(mu))
-        recotype.push_back(mu.algo_type)
-    recomuonsn[0] = len(recomuons)
-
-    tOut.Fill()
-
-print "Write out trig ntuple ", fOut.GetName() , " with ", tOut.GetEntries(), " events"
-fOut.Write()
-fOut.Close()
-exit(0)
+if __name__=='__main__':
+    main()
